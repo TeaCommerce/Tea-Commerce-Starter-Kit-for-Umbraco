1b536b10f667cf76fa9e33e04b3ac0d02e1d3b70
Brian Lyk Larsen <brianlarsen1991@outlook.com>
2015-03-06 10:59:58 +0100

    Product with variants

---
+++ Source/Website/Scripts/product.js
@@ -0,0 +1,12 @@
+﻿jQuery(function() {
+  var variants = jQuery('#variants');
+
+  if (variants[0]) {
+    variants.change(function() {
+      var selectedVariant = jQuery(this).val();
+
+      jQuery('[data-variantid]').addClass('hidden');
+      jQuery('[data-variantid=' + selectedVariant + ']').removeClass('hidden');
+    });
+  }
+});
