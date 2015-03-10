dcecc38ad8d2251f33d1435e95ee09c926940c94
Rune Grønkjær <rg@teasolutions.dk>
2015-03-10 09:57:48 +0100

    Minicart and minor tweaks

---
+++ Source/Website/Scripts/teaCommerceEvents.js
@@ -0,0 +1,25 @@
+﻿var cartUpdateTimeout = null,
+    cartTimeoutStart = null,
+    cartMinimumTimeout = 1000;
+
+TC.bind('afterCartUpdated', function (data, jQForm) {
+  
+  var cartTimeoutEnd = new Date().getTime(),
+      timePast = cartTimeoutEnd - cartTimeoutStart,
+      timeLeft = timePast > cartMinimumTimeout ? 0 : cartMinimumTimeout - timePast,
+      miniCart = jQuery('#minicart');
+
+  cartUpdateTimeout = window.setTimeout(function () {
+    jQForm.add(miniCart).removeClass('loading');
+    if (data.order) {
+      miniCart.find('.quantity').text(data.order.totalQuantity);
+      miniCart.find('.totalPrice').text(data.order.subtotalPrice.value.withVatFormatted);
+    }
+  }, timeLeft);
+});
+
+TC.bind('beforeAddOrUpdateOrderLine', function (data, jQForm) {
+  jQForm.add(jQuery('#minicart')).addClass('loading');
+  cartTimeoutStart = new Date().getTime();
+  window.clearTimeout(cartUpdateTimeout);
+});
