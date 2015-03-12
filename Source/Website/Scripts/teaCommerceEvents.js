d53f21c0eae43179ceea759eabd00f7a31c43914
Rune Grønkjær <rg@teasolutions.dk>
2015-03-12 11:11:27 +0100

    Fixed Go to cart button JavaScript states and jQForm null bug

--- Source/Website/Scripts/teaCommerceEvents.js
+++ Source/Website/Scripts/teaCommerceEvents.js
@@ -3,17 +3,30 @@
     cartMinimumTimeout = 1000;
 
 TC.bind('afterCartUpdated', function (data, jQForm) {
-  
+
   var cartTimeoutEnd = new Date().getTime(),
       timePast = cartTimeoutEnd - cartTimeoutStart,
       timeLeft = timePast > cartMinimumTimeout ? 0 : cartMinimumTimeout - timePast,
       miniCart = jQuery('#minicart');
 
   cartUpdateTimeout = window.setTimeout(function () {
-    jQForm.add(miniCart).removeClass('loading');
+    miniCart.removeClass('loading');
+    if (jQForm && jQForm[0]) {
+      jQForm.removeClass('loading');
+    }
     if (data.order) {
       miniCart.find('.quantity').text(data.order.totalQuantity);
       miniCart.find('.totalPrice').text(data.order.subtotalPrice.value.withVatFormatted);
+      miniCart.removeClass('oneItem empty');
+      if (data.order.totalQuantity > 0) {
+        miniCart.find('a').removeClass('disabled');
+        if (data.order.totalQuantity === 1) {
+          miniCart.addClass('oneItem');
+        }
+      } else if (data.order.totalQuantity === 0) {
+        miniCart.addClass('empty');
+        miniCart.find('a').addClass('disabled');
+      }
     }
   }, timeLeft);
 });
