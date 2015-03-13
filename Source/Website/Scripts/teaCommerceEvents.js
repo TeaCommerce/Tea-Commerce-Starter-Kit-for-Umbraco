3729428af13133e9e733194bef094fcd1619a6e7
Rune Grønkjær <rg@teasolutions.dk>
2015-03-13 11:55:03 +0100

    Added comments in scripts

--- Source/Website/Scripts/teaCommerceEvents.js
+++ Source/Website/Scripts/teaCommerceEvents.js
@@ -1,22 +1,45 @@
-﻿var cartUpdateTimeout = null,
+﻿/**********************************
+  * TEA COMMERCE EVENTS
+***********************************/
+//Global variables
+var cartUpdateTimeout = null,
     cartTimeoutStart = null,
-    cartMinimumTimeout = 1000;
+    cartMinimumTimeout = 1000; //The minimum time to show the user a change is happening. Otherwise he might not get to see it.
 
+//When an orderline is being updated
+TC.bind('beforeAddOrUpdateOrderLine', function (data, jQForm) {
+  //Start loading the minicart
+  jQForm.add(jQuery('#minicart')).addClass('loading');
+  //Set start time for the change event
+  cartTimeoutStart = new Date().getTime();
+  //Clear any old cart update timeouts
+  window.clearTimeout(cartUpdateTimeout);
+});
+
+//After the cart have been updated
 TC.bind('afterCartUpdated', function (data, jQForm) {
 
-  var cartTimeoutEnd = new Date().getTime(),
-      timePast = cartTimeoutEnd - cartTimeoutStart,
-      timeLeft = timePast > cartMinimumTimeout ? 0 : cartMinimumTimeout - timePast,
+  var cartTimeoutEnd = new Date().getTime(), //Set end time for the change event
+      timePast = cartTimeoutEnd - cartTimeoutStart,//Calculate the time since start
+      timeLeft = timePast > cartMinimumTimeout ? 0 : cartMinimumTimeout - timePast, //Calculate the time left to show loading status
       miniCart = jQuery('#minicart');
 
+  //Set a timeout for the remainder of the time
   cartUpdateTimeout = window.setTimeout(function () {
+    //Remove loading status from the mini cart
     miniCart.removeClass('loading');
     if (jQForm && jQForm[0]) {
+      //Remove loading status from the form if there is one
       jQForm.removeClass('loading');
     }
+    //If an order have been returned we need to update the UI
     if (data.order) {
+      //Update quantity
       miniCart.find('.quantity').text(data.order.totalQuantity);
+      //Update the total price
       miniCart.find('.totalPrice').text(data.order.subtotalPrice.value.withVatFormatted);
+
+      //Update css classes on the minicart depending on the order status
       miniCart.removeClass('oneItem empty');
       if (data.order.totalQuantity > 0) {
         miniCart.find('a').removeClass('disabled');
@@ -29,10 +52,4 @@ TC.bind('afterCartUpdated', function (data, jQForm) {
       }
     }
   }, timeLeft);
-});
-
-TC.bind('beforeAddOrUpdateOrderLine', function (data, jQForm) {
-  jQForm.add(jQuery('#minicart')).addClass('loading');
-  cartTimeoutStart = new Date().getTime();
-  window.clearTimeout(cartUpdateTimeout);
-});
+}); 
