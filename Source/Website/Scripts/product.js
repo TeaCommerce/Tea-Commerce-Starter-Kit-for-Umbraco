c97a7f0a1400c05712abebf6924726455e506a9d
Rune Gronkjaer <rg@teasolutions.dk>
2016-02-29 13:20:43 +0100

    Updated Tea Commerce and added support for new variant structure

--- Source/Website/Scripts/product.js
+++ Source/Website/Scripts/product.js
@@ -2,17 +2,131 @@
   * PRODUCT
 ***********************************/
 jQuery(function () {
-  //Get variant on page
-  var variants = jQuery('#variants');
-  if (variants[0]) {
-    
-    //When variants are selected by the user we change the UI
-    variants.change(function() {
-      var selectedVariant = jQuery(this).val();
-      //Hide all variant information
-      jQuery('[data-variantid]').addClass('hidden');
-      //Show variant information for the selected variant
-      jQuery('[data-variantid=' + selectedVariant + ']').removeClass('hidden');
-    });
+
+  if (_products) {
+
+    function variantChange(event) {
+      var select = jQuery(this),
+          form = select.closest('form'),
+          productId = parseInt(form.attr('data-product-id')),
+          attributeGroups = form.find('select[data-attribute-group]'),
+          productIdentifierField = form.find('[name=productIdentifier]'),
+          submitButtons = form.find('[type="submit"]'),
+          reachedEnd = false,
+          reachedInitiator = false,
+          filters = [],
+          initiator = event.target;
+
+      if (productId) {
+        var filteredVariants = jQuery.extend({}, _products[productId]); //Avoid keeping a reference to the original variants object
+
+        //Reset select status
+        attributeGroups.each(function () {
+          var attributeGroup = jQuery(this);
+
+          if (reachedInitiator) {
+            attributeGroup.val('');
+          }
+
+          //Hide and reset all elements after an empty select
+          if (reachedEnd) {
+            attributeGroup.prop('disabled', true);
+            attributeGroup.val('');
+          } else {
+            attributeGroup.prop('disabled', false);
+
+            if (attributeGroup.val()) {
+              filters.push(attributeGroup.val());
+            }
+          }
+
+          if (attributeGroup.val() == '') {
+            reachedEnd = true;
+          }
+
+          if (attributeGroup.is(initiator)) {
+            reachedInitiator = true;
+          }
+        });
+
+        //Find all potential variants with the given filters
+        jQuery.each(filteredVariants, function (id, variant) {
+          for (var i = 0; i < filters.length; i++) {
+            var filter = parseInt(filters[i]);
+
+            if (jQuery.inArray(filter, variant.combinations) == -1) {
+              delete filteredVariants[id];
+            }
+          }
+        });
+
+        //Disable options with no variants
+        attributeGroups.each(function () {
+          var attributeGroup = jQuery(this),
+              options = attributeGroup.find('option');
+
+          if (!attributeGroup.val()) {
+            options.each(function () {
+              var option = jQuery(this);
+
+              if (option.val()) {
+                option.prop('disabled', true);
+
+                jQuery.each(filteredVariants, function (id, variant) {
+                  if (jQuery.inArray(parseInt(option.val()), variant.combinations) > -1) {
+
+                    option.prop('disabled', false);
+
+                    return false;
+                  }
+                });
+              }
+            });
+            var optionsLeft = options.filter('[value!=""]:not(:disabled)');
+            if (optionsLeft.length == 1) {
+              attributeGroup.val(optionsLeft.val());
+              attributeGroup.trigger('change');
+            }
+          }
+
+        });
+
+        if (filteredVariants && Object.keys(filteredVariants).length === 1) {
+          //One single variant is found
+          var variantId = getFirstKey(filteredVariants),
+              variant = filteredVariants[variantId],
+              productIdentifier = variant.productIdentifier;
+          productIdentifierField.val(productIdentifier);
+          submitButtons.removeClass('disabled');
+          //INSERT CODE HERE
+
+        } else {
+          productIdentifierField.val('');
+          submitButtons.addClass('disabled');
+        }
+      }
+    }
+
+    function initVariants() {
+      jQuery('form[action="/base/TC/FormPost.aspx"]').each(function () {
+        var form = jQuery(this),
+            attributeGroups = form.find('select[data-attribute-group]');
+        if (attributeGroups[0]) {
+          attributeGroups.not(':first').prop('disabled', true);
+
+          attributeGroups.on('change', variantChange);
+          attributeGroups.first().trigger('change');
+        }
+      });
+
+    }
+
+    function getFirstKey(data) {
+      for (var prop in data)
+        if (data.propertyIsEnumerable(prop))
+          return prop;
+    }
+
+    initVariants();
   }
 });
