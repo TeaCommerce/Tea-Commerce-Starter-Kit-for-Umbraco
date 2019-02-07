f6ac54560196121bb4a39ecdf49032d2305116f9
mattbrailsford <me@mattbrailsford.com>
2019-02-07 10:05:13 +0000

    Updated copyright year

--- Source/Website/Properties/AssemblyInfo.cs
+++ Source/Website/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Tea_Commerce___Starter_kit_for_Umbraco" )]
-[assembly: AssemblyCopyright( "Copyright ©  2015" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
